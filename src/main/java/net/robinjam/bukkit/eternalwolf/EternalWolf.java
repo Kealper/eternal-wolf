0f6b4dc99b645f86452ba3d297802e382633ecd7
James Robinson <robinjam304@hotmail.com>
2011-06-05 14:54:23 +0100

    Create project structure

---
+++ src/main/java/net/robinjam/bukkit/eternalwolf/EternalWolf.java
@@ -0,0 +1,19 @@
+package net.robinjam.bukkit.eternalwolf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author robinjam
+ */
+public class EternalWolf extends JavaPlugin {
+
+    public void onDisable() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void onEnable() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
