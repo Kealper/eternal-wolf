2e841c5f94cd06db604790b12fd97fd1c4e7b986
James Robinson <robinjam304@hotmail.com>
2011-06-05 16:11:24 +0100

    Enforce maximum of 5 wolves per player

--- src/main/java/net/robinjam/bukkit/eternalwolf/EternalWolf.java
+++ src/main/java/net/robinjam/bukkit/eternalwolf/EternalWolf.java
@@ -12,11 +12,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class EternalWolf extends JavaPlugin {
 
     EntityListener entityListener = new EntityListener();
+    PlayerListener playerListener = new PlayerListener();
 
     public void onEnable() {
         // Register events
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvent(Event.Type.ENTITY_DAMAGE, entityListener, Event.Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_INTERACT_ENTITY, playerListener, Event.Priority.Normal, this);
 
         // Read plugin description file
         PluginDescriptionFile pdf = this.getDescription();
---
+++ src/main/java/net/robinjam/bukkit/eternalwolf/PlayerListener.java
@@ -0,0 +1,49 @@
+package net.robinjam.bukkit.eternalwolf;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+/**
+ *
+ * @author robinjam
+ */
+class PlayerListener extends org.bukkit.event.player.PlayerListener {
+
+    @Override
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        event.setCancelled(false);
+        Player player = event.getPlayer();
+        Entity target = event.getRightClicked();
+
+        // If the player is giving a bone to a wolf
+        if (player.getItemInHand().getType() == Material.BONE && 
+                event.getRightClicked() instanceof Wolf) {
+
+            // If the wolf is wild and the player already owns too many wolves,
+            // do not allow them to tame another one
+            if (!((Wolf)target).isTamed() && playerHasTooManyWolves(player)) {
+                event.setCancelled(true);
+                player.sendMessage(ChatColor.RED + "You cannot tame any more wolves!");
+            }
+        }
+    }
+
+    protected boolean playerHasTooManyWolves(Player player) {
+        int numWolves = 0;
+        for (Entity entity : player.getWorld().getEntities()) {
+            if (entity instanceof Wolf) {
+                Wolf wolf = (Wolf) entity;
+
+                if (wolf.getOwner() == player) {
+                    numWolves++;
+                }
+            }
+        }
+
+        return (numWolves >= 5);
+    }
+}
